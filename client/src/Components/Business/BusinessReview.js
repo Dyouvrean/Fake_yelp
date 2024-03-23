460cc9b5f70852d6df080d4145a2715d36652aeb
Dyouvrean <dry199984@outlook.com>
2024-03-23 03:36:16 -0400

    Pagination of reviews

--- client/src/Components/Business/BusinessReview.js
+++ client/src/Components/Business/BusinessReview.js
@@ -6,20 +6,53 @@ import axios from 'axios';
 import APIService from '../../api/ApiService';
 import { useParams } from 'react-router-dom';
 import ReviewCard from './ReviewCard';
-
+import { FaAngleLeft } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa";
 
 const BusinessReview = ({Business_id}) => {
-    
+
+    const ReviewsPerPage = 10;
     const [reviewlist, setreviewlist] =useState([])
+    const [currentPage, setCurrentPage] = useState(1);
+    const [total_page, setTotal] = useState(0)
+    var indexOfLastReview = currentPage * ReviewsPerPage;
+    var indexOfFirstReview = indexOfLastReview - ReviewsPerPage;
+    var currentReviews = reviewlist.slice(indexOfFirstReview, indexOfLastReview);
 
     useEffect(() => {
         const data = {
         BusId: Business_id
         };
-        APIService.Get_business_review(data).then((res)=>{setreviewlist(res.data)
+        APIService.Get_business_review(data).then((res)=>{
+            setreviewlist(res.data.sort((a, b) => new Date(b.date) - new Date(a.date)));
+            setTotal(Math.ceil(res.data.length / ReviewsPerPage))
             console.log(res.data);
+            console.log(Math.ceil(res.data.length / ReviewsPerPage))
         });
       }, []);
+
+  useEffect(()=>{
+    indexOfLastReview = currentPage * ReviewsPerPage;
+    indexOfFirstReview = indexOfLastReview - ReviewsPerPage;
+    currentReviews = reviewlist.slice(indexOfFirstReview, indexOfLastReview);
+
+  },[currentPage])
+
+   const  handlePageReduce =() =>{
+      if ((currentPage-1)<1){
+         setCurrentPage(1);
+      }
+      else{
+        setCurrentPage(currentPage-1);
+      }
+    }
+
+
+   const handlePageIncrease =()=>{
+      if ((currentPage+1)<=total_page){
+        setCurrentPage(currentPage+1);
+     }
+    }
 
       return (
         <div>
@@ -27,14 +60,19 @@ const BusinessReview = ({Business_id}) => {
            Review:
            </div>
            <div>
-           {reviewlist.sort((a, b) => new Date(b.date) - new Date(a.date)).map((result)=>{
+           {currentReviews.map((result)=>{
             console.log(result)
             return <ReviewCard info= {result}/>
           })
           }
            </div>
-        </div>
 
+           <div style={{ textAlign: 'center' }}>
+           {((currentPage-1)>1) && <FaAngleLeft onClick={handlePageReduce}/>}
+           {((currentPage+1)<=total_page)&& <FaAngleRight onClick={handlePageIncrease}/>}
+           </div>
+        </div>
+        
 
       )
 
